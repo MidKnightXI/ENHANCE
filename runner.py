06f11a5548e663eeefb655698473f5e30885a9b3
MidKnightXI <romain.bouguet@epitech.eu>
2023-12-28 10:27:06 +0100

    feat: now make runner accept a directory as input

--- runner.py
+++ runner.py
@@ -1,34 +1,34 @@
+import os
+import argparse
 import torch
 from torch import nn
 from torchvision.utils import save_image
-from os import path
 from torchvision import transforms
 from PIL import Image
-import matplotlib.pyplot as plt
 
 class DenoisingAutoencoder(nn.Module):
     def __init__(self):
         super(DenoisingAutoencoder, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size = (3,3), padding = "same"),
+            nn.Conv2d(3, 32, kernel_size=(3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2,2), padding = 0),
-            nn.Conv2d(32, 64, kernel_size = (3,3), padding = "same"),
+            nn.MaxPool2d((2, 2), padding=0),
+            nn.Conv2d(32, 64, kernel_size=(3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2,2), padding = 0),
-            nn.Conv2d(64, 128, kernel_size = (3,3), padding = "same"),
+            nn.MaxPool2d((2, 2), padding=0),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2,2), padding = 0))
+            nn.MaxPool2d((2, 2), padding=0))
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 128, kernel_size = (3,3), stride = 2, padding = 0),
+            nn.ConvTranspose2d(128, 128, kernel_size=(3, 3), stride=2, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size = (3,3), stride = 2, padding = 0),
+            nn.ConvTranspose2d(128, 64, kernel_size=(3, 3), stride=2, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size = (3,3), stride = 2, padding = 0),
+            nn.ConvTranspose2d(64, 32, kernel_size=(3, 3), stride=2, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 3, kernel_size = (3,3), stride = 1, padding = 1),
+            nn.ConvTranspose2d(32, 3, kernel_size=(3, 3), stride=1, padding=1),
             nn.Sigmoid())
 
     def forward(self, x):
@@ -36,39 +36,48 @@ class DenoisingAutoencoder(nn.Module):
         x = self.decoder(x)
         return x
 
-# Instantiate the model
-model = DenoisingAutoencoder()
-model.load_state_dict(torch.load('denoising_model.pth'))
-model.eval()
+def setup_argparse():
+    parser = argparse.ArgumentParser(description='Denoise images using a pre-trained autoencoder model.')
+    parser.add_argument(
+        'input_directory',
+        type=str,
+        help='Path to the input directory containing images for denoising.')
+    return parser.parse_args()
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
+def denoise_images_in_directory(model, input_directory, output_directory):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
 
-image_path = '/Users/midknight/Downloads/Why+are+my+photos+grainy+3.jpg'
-output_path = path.splitext(image_path)[0] + '_denoised.jpg'
+    os.makedirs(output_directory, exist_ok=True)
 
-sample_image = Image.open(image_path).convert("RGB")
+    for filename in os.listdir(input_directory):
+        if filename.endswith(('.jpg', '.jpeg', '.png')):
+            image_path = os.path.join(input_directory, filename)
+            output_path = os.path.join(
+                output_directory,
+                f"{os.path.splitext(filename)[0]}_denoised.jpg")
 
-# Preprocess the image
-input_image = transform(sample_image).unsqueeze(0)  # Add batch dimension
+            sample_image = Image.open(image_path).convert("RGB")
 
-# Denoise the image using the trained model
-with torch.no_grad():
-    denoised_image = model(input_image)
+            input_image = transform(sample_image).unsqueeze(0)
 
-save_image(denoised_image, output_path)
+            with torch.no_grad():
+                denoised_image = model(input_image)
 
-# Display the original and denoised images
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.title('Original Image')
-plt.imshow(sample_image)
-plt.axis('off')
+            save_image(denoised_image, output_path)
 
-plt.subplot(1, 2, 2)
-plt.title('Denoised Image')
-plt.imshow(denoised_image.squeeze(0).permute(1, 2, 0).numpy())
-plt.axis('off')
+def main():
+    args = setup_argparse()
 
-plt.show()
+    model = DenoisingAutoencoder()
+    model.load_state_dict(torch.load('denoising_model.pth'))
+    model.eval()
+
+    input_directory = args.input_directory
+    output_directory = os.path.join(input_directory, 'denoised_output')
+
+    denoise_images_in_directory(model, input_directory, output_directory)
+
+if __name__ == "__main__":
+    main()

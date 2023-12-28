6c312d5d05d1448ce30bcd729d812c774dc2b1a8
MidKnightXI <romain.bouguet@epitech.eu>
2023-12-28 10:17:33 +0100

    feat: improving code readability

--- trainer.py
+++ trainer.py
@@ -1,5 +1,4 @@
 import torch
-import torch.backends.mps
 import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
@@ -24,15 +23,12 @@ class DenoisingDataset(Dataset):
 
         image = Image.open(img_path).convert('RGB')
 
-        # Resize the image to the target size
         image = image.resize(self.target_size, Image.BICUBIC)
 
-        # Apply transformations if provided
         if self.transform:
             image = self.transform(image)
 
         return {'image': image, 'label': label}
-
 
 
 class DenoisingAutoencoder(nn.Module):
@@ -40,24 +36,24 @@ class DenoisingAutoencoder(nn.Module):
         super(DenoisingAutoencoder, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size = (3,3), padding = "same"),
+            nn.Conv2d(3, 32, kernel_size=(3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2,2), padding = 0),
-            nn.Conv2d(32, 64, kernel_size = (3,3), padding = "same"),
+            nn.MaxPool2d((2, 2), padding=0),
+            nn.Conv2d(32, 64, kernel_size=(3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2,2), padding = 0),
-            nn.Conv2d(64, 128, kernel_size = (3,3), padding = "same"),
+            nn.MaxPool2d((2, 2), padding=0),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2,2), padding = 0))
+            nn.MaxPool2d((2, 2), padding=0))
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 128, kernel_size = (3,3), stride = 2, padding = 0),
+            nn.ConvTranspose2d(128, 128, kernel_size=(3, 3), stride=2, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size = (3,3), stride = 2, padding = 0),
+            nn.ConvTranspose2d(128, 64, kernel_size=(3, 3), stride=2, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size = (3,3), stride = 2, padding = 0),
+            nn.ConvTranspose2d(64, 32, kernel_size=(3, 3), stride=2, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 3, kernel_size = (3,3), stride = 1, padding = 1),
+            nn.ConvTranspose2d(32, 3, kernel_size=(3, 3), stride=1, padding=1),
             nn.Sigmoid())
 
     def forward(self, x):
@@ -66,38 +62,62 @@ class DenoisingAutoencoder(nn.Module):
         return x
 
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
+def setup_device():
+    device = torch.device("cpu")
 
-train_dataset = DenoisingDataset(csv_path='/Users/midknight/perso/ENHANCE/dataset/dataset_info.csv', transform=transform)
-data_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    # if torch.cuda.is_available(): # Uncomment for Nvidia GPUs
+    #     DEVICE = torch.device("cuda")
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
 
-device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
-model = DenoisingAutoencoder().to(device)
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-num_epochs = 10
-
-for epoch in range(num_epochs):
-    total_loss = 0.0
-
-    for batch_idx, batch in enumerate(data_loader):
-        inputs = batch['image'].to(device)
-        targets = batch['image'].to(device)
-
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        total_loss += loss.item()
-
-    average_loss = total_loss / len(data_loader)
-    stdout.write(f'Epoch [{epoch+1}/{num_epochs}], Average Loss: {average_loss:.4f}\n')
+    stdout.write(f"Training on {device}")
     stdout.flush()
+    return device
 
-model.to(torch.device("cpu"))
-torch.save(model.state_dict(), "denoising_model.pth")
+
+def train_model(model, data_loader, criterion, optimizer, device, num_epochs=10):
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+
+        for batch_idx, batch in enumerate(data_loader):
+            inputs = batch['image'].to(device)
+            targets = batch['image'].to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        average_loss = total_loss / len(data_loader)
+        stdout.write(f'Epoch [{epoch+1}/{num_epochs}], Average Loss: {average_loss:.4f}\n')
+        stdout.flush()
+
+    model.to(torch.device("cpu"))
+    torch.save(model.state_dict(), "denoising_model.pth")
+
+
+def main():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_dataset = DenoisingDataset(csv_path='./dataset/dataset_info.csv',
+                                     transform=transform)
+    data_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+    DEVICE = setup_device()
+    EPOCH = 10
+
+    model = DenoisingAutoencoder().to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+    train_model(model, data_loader, criterion, optimizer, DEVICE, EPOCH)
+
+
+if __name__ == "__main__":
+    main()
